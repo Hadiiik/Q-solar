627e9325f07c5d8b0c3c05adf86ab881e42ce80b
hadiikaddour <hadyqdwr77@gmail.com>
2026-03-24 19:29:20 +0300

    fix

--- register-sw.js
+++ register-sw.js
@@ -1,7 +1,7 @@
 // register-sw.js
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/sw.js')
             .then((registration) => {
                 console.log('Service Worker registered successfully:', registration);
                 
